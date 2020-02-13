956098e1554125daa5f1a1500984bb250dc45ae1
Roma <roma@roma.so>
2020-02-13 17:33:14 +0300

    remove console

--- front/src/store/modules/users.js
+++ front/src/store/modules/users.js
@@ -18,7 +18,7 @@ const getters = {
 // actions
 const actions = {
   fetchUsers ({ commit, state }) {
-    axios.get(`/users/`)
+    axios.get(`/users/users.json`)
       .then((response) => {
         if (response) {
           commit('SET_USERS', response)
@@ -37,8 +37,7 @@ const mutations = {
   ['FILTER_USERS'] (stateFreezed, search) {
     stateFreezed.search = search.toLowerCase()
     if (stateFreezed.search) {
-      stateFreezed.users = stateFreezed.data.filter((user, key) => {
-        if (key === 0) console.log(JSON.stringify(Object.values(user)).toLowerCase())
+      stateFreezed.users = stateFreezed.data.filter((user) => {
         return JSON.stringify(Object.values(user)).toLowerCase().indexOf(stateFreezed.search) > -1
       })
     } else stateFreezed.users = stateFreezed.data
